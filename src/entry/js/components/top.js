f26d0b18ca404df8f8246e60276480a5988eb22a
Yuuna Yanagida <66319933+y-yana@users.noreply.github.com>
2022-02-25 00:12:36 +0900

    add sound func

--- src/entry/js/components/top.js
+++ src/entry/js/components/top.js
@@ -1,1 +1,17 @@
-console.log('top')
+const top = new Vue({
+  el: '#topPage',
+  // FlaskとVueを共存させるためにDelimiterを変更する
+  delimiters: ["[[", "]]"],
+  data: {
+    info: null
+  },
+  methods: {
+    soundPlay: function () {
+      const helloVoice = new Audio("./static/sound/checkbox.mp3");
+      var soundCheck = document.getElementById("sound").checked
+      if (soundCheck == true) {
+        helloVoice.play();
+      }
+    }
+  }
+});
