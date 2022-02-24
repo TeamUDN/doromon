5f9f24e2928643d0fdbf4478d6e39fc209c64235
Ryunosuke-Ikeda <ryunosuke0305@gmail.com>
2022-02-24 20:13:38 +0900

    read_images

--- src/app.py
+++ src/app.py
@@ -1,4 +1,8 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+import base64
+from PIL import Image
+import io 
+import numpy as np
 
 app = Flask(__name__)
 
@@ -11,6 +15,20 @@ def index():
 @app.route('/draw')
 def draw():
     return render_template("draw.html")
+
+
+@app.route('/draw_post', methods=['POST'])
+def set_data():
+    enc_data  = request.form['img']
+    #dec_data = base64.b64decode( enc_data )              # これではエラー  下記対応↓
+    dec_data = base64.b64decode( enc_data.split(',')[1] )# 環境依存の様(","で区切って本体をdecode)
+    img  = Image.open(io.BytesIO(dec_data)).convert("L") 
+
+    img=img.resize((28,28))
+    data=np.asarray(img)
+    data=data-255
+
+    print(data.shape)
 
 if __name__ == '__main__':
   app.run()
